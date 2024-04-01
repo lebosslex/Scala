b68f12e3b7c288c31aadcd9e36befa82dc1aabc0
lexleboss <lebosslex64@gmail.com>
2024-04-01 10:21:41 +0200

    init

--- Condition.scala
+++ Condition.scala
@@ -1,8 +1,12 @@
 @main def condition () =
-  val x = 1
+  val a = 2
+  val b = 2
+  val x = if a < b then a else b
   if  x < 0 then
     println("negative")
   else if x == 0 then
     println("zero")
   else
     println("positive")
+
+  println(x)
